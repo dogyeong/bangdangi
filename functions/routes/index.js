2438cdec4009b5591a83fdb7948b36297fdfaf97
dogyeong <shroad1802@naver.com>
2019-10-04 16:09:28 +0900

    메인페이지 완성?

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const admin = require('firebase-admin');
+const createError = require('http-errors');
+const url = require('url');
+const cors = require('cors')({
+    origin: true
+});
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -7,8 +13,31 @@ router.get('/', (req, res, next) => {
   res.send("Hi from index routes!")
 });
 
-router.get('test', (req, res, next) => {
-    res.send('test');
+
+router.use('/test', (req, res, next) => {
+  res.send('test');
 });
+
+router.get('/getNew', (req, res) => {
+  let ref = admin.firestore().doc('article/live');
+  ref.getCollections().then(collections => {
+      let arr = [];
+      return collections.forEach(collection => {
+        return res.send(JSON.parse(getArticleList(collection)));
+      });
+  }).catch(error => console.log(error)); 
+  res.send('getNew OK');
+})
+
+async function getArticleList(collection) {
+  let arr = [];
+  await collection.where('done', '==', false).where('display', '==', true).get()
+  .then(docs => {
+    return arr = docs.map(doc => doc.data());
+  })
+  .catch(err => console.log(err));
+
+  return arr;
+}
 
 module.exports = router;
