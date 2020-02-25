bc9f90d0515c95d02e0697ef6a9410fde888b557
dogyeong <shroad1802@naver.com>
2020-02-25 11:36:40 +0900

    가계약 페이지 제작중

--- functions/routes/reservation.js
+++ functions/routes/reservation.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const admin = require('firebase-admin');
 const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const url = require('url');
+const nodemailer = require('nodemailer');
+const smtpPool = require('nodemailer-smtp-pool');
 const cors = require('cors')({
     origin: true
 });
@@ -21,6 +20,51 @@ const PLACE_OBJ = {
 }
 const getArticlesPath = (place) => `article/${place}/articles`;
 const getLocKeywordsPath = (place) => `article/${place}/keywords/locationKeywords`;
+
+router.post('/payments', (req, res, next) => {
+    var transporter = nodemailer.createTransport(smtpPool({
+        service: 'gmail',
+        auth: {
+            user: 'shroad1802@gmail.com',
+            pass: process.env.GMAIL_PASSWORD
+        }
+      }));
+
+    const mailOptions = {
+        from: '방단기 <shroad1802@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+        to: 'shroad1802@naver.com',
+        subject: '새 계약요청', // email subject
+        html: `<p style="font-size: 16px;">지역 : ${req.body.place}</p><br />
+            <p style="font-size: 16px;">매물id : ${req.body.articleId}</p><br />
+            <p style="font-size: 16px;">이름 : ${req.body.name}</p><br />
+            <p style="font-size: 16px;">휴대폰번호 : ${req.body.phone}</p><br />
+        ` 
+    };
+
+
+
+    db.collection('reservation').add({
+        place: req.body.place,
+        articleId: req.body.articleId,
+        name: req.body.name,
+        phone: req.body.phone,
+        startDate: new Date(req.body.start_date),
+        endDate: new Date(req.body.end_date),
+        createdAt: new Date(),
+    })
+    .then(() => {
+        return transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return res.send(err.toString());
+            }
+            return res.send('OK');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        return next(createError(500));
+    });
+});
 
 router.get('/:place/:articleId', (req, res, next) => { 
     const place = req.params.place;
